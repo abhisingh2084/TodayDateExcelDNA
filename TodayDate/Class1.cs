d1dc7631df6df98327fa1eea40145d97c3ca3a77
Abhishek Kumar <Abhishek Kumar@ABHISHEK-KUMAR->
2024-05-22 11:17:41 +0530

    Add project files.

---
+++ TodayDate/Class1.cs
@@ -0,0 +1,21 @@
+﻿using ExcelDna.Integration;
+using System;
+
+public class ExcelFunctions
+{
+    [ExcelFunction(Name = "Func.Date", Description = "Returns today's date")]
+    public static object GetToday()
+    {
+        return DateTime.Today;
+    }
+
+    [ExcelFunction(Name = "Func.2DArray", Description = "Return a 2x2 Array")]
+
+    public static object[,] Get2DArray()
+    {
+        return new object[,] { { 1, 2 }, { 3, 4 } };
+    }
+
+     
+}
+  
